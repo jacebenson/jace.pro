3256afc0cebe996e10e6c5c74ee9a2dc4fb97f45
Jace Benson <jace.benson@protonmail.com>
2020-03-22 01:25:12 -0500

    got it

--- scripts/buildsnblogs.js
+++ scripts/buildsnblogs.js
@@ -4,18 +4,17 @@ var outputArr = [];
 //https://community.servicenow.com/
 //api/sn_communities/v1/community/contents?
 //
-var getNowBlogs = function(urlParams, callback){
-    if(!urlParams){
-        urlParams = {
-            last: 20,
-            stFrom:0
-        }
+var getNowBlogs = function(start, callback){
+    if(!start){
+        start = 0;
     }
+    console.log('getting blogs starting at ', start)
+    var end = parseInt(start,10) + 100;
     var options = {
         method: "GET",
         hostname: "community.servicenow.com",
         //path: "/api/sn_communities/v1/community/contents?last=80&stFrom=60&before=&forum=a6299a2ddbd897c068c1fb651f961926&type=cc3fcaa0dbd26600b1f6f78eaf96192e&sort=publish&filters=undefined",
-        path: "/api/sn_communities/v1/community/contents?last="+urlParams.last+"&stFrom="+urlParams.stFrom+"&before="+new Date().toISOString()+"&forum=a6299a2ddbd897c068c1fb651f961926&type=cc3fcaa0dbd26600b1f6f78eaf96192e&sort=publish&filters=undefined",
+        path: "/api/sn_communities/v1/community/contents?last="+end+"&stFrom="+start+"&before="+new Date().toISOString()+"&forum=a6299a2ddbd897c068c1fb651f961926&type=cc3fcaa0dbd26600b1f6f78eaf96192e&sort=publish&filters=undefined",
         headers: {
             "Accept": "*/*",
             "Cache-Control": "no-cache",
@@ -39,50 +38,42 @@ var getNowBlogs = function(urlParams, callback){
             var body = Buffer.concat(chunks);
             //console.log(body.toString());
             var responseObj = JSON.parse(body);
-            //console.log(JSON.stringify(responseObj.result, '', ' '));
-            //console.log('all feeds loaded, writing to feeds.json');
-            // loop over the contens and distill it to;
-            // author
-            // link
-            // date nnnTdddZ format
-            // 
             
             responseObj.result.contents.forEach(function(post){
                 var dateObj = new Date(post.published_date);
                 outputArr.push ({
                     date: dateObj.toISOString(),
-                    site: "ServiceNow Community Blogs",
+                    site: "ServiceNow Blogs",
                     category: "Blog",
                     title: post.title,
                     author: post.userAvatarObject.name,
                     link: "https://community.servicenow.com/community?id=community_blog&sys_id=" + post.sys_id
-                    //https://community.servicenow.com/community?id=community_blog&sys_id=bed58beb1ba3c8d4fff162c4bd4bcb89 
-                    //https://community.servicenow.com/sn_communities_blog.do?sys_id=bed58beb1ba3c8d4fff162c4bd4bcb89
-                    //
                 });
             });
-            if(responseObj.hasMoreRecords){
-                getNowBlogs({
-                    last:parseInt(responseObj.nextRecord,10) + 20,
-                    stFrom:parseInt(responseObj.nextRecord,10)
-                },null);
+            console.log('responseObj.hasMoreRecords',responseObj.result.hasMoreRecords, responseObj.result.contents[0].published_date, responseObj.result.contents[0].title);
+            if(responseObj.result.hasMoreRecords){
+                getNowBlogs(parseInt(responseObj.result.nextRecord,10),function(){
+                    console.log('outputArr.length',outputArr.length)
+                });
+            } else {
+                callback();
+                //function(){
+                    fs.readFile('./data/feeds.json', (err, data) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log(data);
+                        var tempData = JSON.parse(data);
+                        var newArr = tempData.concat(outputArr);
+                        var uniqueArray = newArr.filter(function(item, pos) {
+                            return newArr.indexOf(item) == pos;
+                        })
+                        fs.writeFileSync('./data/feeds.json', JSON.stringify(uniqueArray, '', ' '));
+                      });
+                //}
             }
-            callback();
         });
     });
     req.end(); 
 }
-getNowBlogs(null,function(){
-    fs.readFile('./data/feeds.json', (err, data) => {
-        if (err) {
-            throw err;
-        }
-        console.log(data);
-        var tempData = JSON.parse(data);
-        var newArr = tempData.concat(outputArr);
-        var uniqueArray = newArr.filter(function(item, pos) {
-            return newArr.indexOf(item) == pos;
-        })
-        fs.writeFileSync('./data/feeds.json', JSON.stringify(uniqueArray, '', ' '));
-      });
-});
+getNowBlogs(null,null);
