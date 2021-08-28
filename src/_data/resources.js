06aed459f2709cef8c9385d14fa6e6b54f3f8578
Jace <638764+jacebenson@users.noreply.github.com>
2021-08-27 23:14:19 -0500

    updatd more links

--- src/_data/resources.js
+++ src/_data/resources.js
@@ -13,10 +13,10 @@ module.exports = {
     "Shared Code and Labs": [
         { "text": "Dev-Labs.io", "link": "https://dev-labs.io/" },
         { "text": "Share", "link": "https://developer.servicenow.com/app.do#!/share" },
-        { "text": "K18 Lab Guides", "link": "https://developer.servicenow.com/app.do#!/event/knowledge18" },
-        { "text": "K18 CreatorCon Lab Guides", "link": "https://developer.servicenow.com/app.do#!/event/creatorcon18" },
-        { "text": "K19 Labs and CCWs", "link": "https://developer.servicenow.com/app.do#!/event/knowledge19" },
-        { "text": "Now at work '19 Lab Guides", "link": "https://developer.servicenow.com/app.do#!/event/naw19" }
+        { "text": "K18 Lab Guides", "link": "https://developer.servicenow.com/connect.do#!/event/knowledge18" },
+        { "text": "K18 CreatorCon Lab Guides", "link": "https://developer.servicenow.com/connect.do#!/event/creatorcon18" },
+        { "text": "K19 Labs and CCWs", "link": "https://developer.servicenow.com/connect.do#!/event/knowledge19" },
+        { "text": "Now at work '19 Lab Guides", "link": "https://developer.servicenow.com/connect.do#!/event/naw19" }
     ],
     "Community": [
         {"text": "StackOverflow", "link": "https://stackoverflow.com/questions/tagged/servicenow"},
@@ -26,8 +26,8 @@ module.exports = {
         {"text": "Slack", "link": "https://sndevs.slack.com/"}
     ],
     "Documentation": [
-        {"text": "developer.servicenow.com", "link": "https://developer.servicenow.com/app.do#!/api_doc"},
-        {"text": "docs.servicenow.com", "link": "https://docs.servicenow.com/bundle/london-application-development/page/build/applications/concept/api-javascript.html"},
+        {"text": "developer.servicenow.com", "link": "https://developer.servicenow.com/"},
+        {"text": "docs.servicenow.com", "link": "https://docs.servicenow.com/"},
         {"text": "Style Guide How", "link": "https://hi.service-now.com/styles/heisenberg/styleguide/docs/index.html"},
         {"text": "Retina Icons", "link": "https://hi.service-now.com/styles/retina_icons/retina_icons.html"},
         {"text": "Angular 1.5.x", "link": "https://code.angularjs.org/1.5.3/docs/api"},
